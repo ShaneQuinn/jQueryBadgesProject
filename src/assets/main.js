7e5be237e3ad437be641f6035743af82d766f80c
Shane Quinn <shane.quinn@intel.com>
2017-07-26 08:32:44 -0700

    Course completed

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,38 @@
 $(function() {
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2334931.json',
+    datatype: 'jsonp',
+    success: function(response) {
+      // Had to make a slight alteration due to no completed courses.
+      courseData = response.courses.in_progress;
+      makeCourses(courseData);
+      // console.log('courseData', courseData);
+    }
+  })
 
-  // your code will go here
+  function makeCourses(courses) {
+    var $badges = $('#badges');
 
+    courses.forEach(function(course) {
+      var $course = $('<div />', {
+        class: 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        href: course.url,
+        target: '_blank',
+        class: 'btn btn-primary',
+        text: 'See Course'
+      }).appendTo($course);
+
+    })
+  }
 });
